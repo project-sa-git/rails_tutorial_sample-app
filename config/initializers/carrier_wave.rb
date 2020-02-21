d30b3ef9db4001de3019a014469b6ecf92810186
EC2 Default User <ec2-user@ip-172-31-41-189.ap-northeast-1.compute.internal>
2020-02-21 16:12:40 +0000

    fixed conflict

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,28 +1,20 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-<<<<<<< HEAD
-    # config.storage :fog #=> 追加
+    config.storage :fog #=> 追加
     config.fog_provider = 'fog/aws'  #=> 追加
-=======
-    config.fog_provider = 'fog/aws' 
->>>>>>> user-microposts
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'], 
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
-<<<<<<< HEAD
     # キャッシュの保存期間
-=======
->>>>>>> user-microposts
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } 
+    config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
   end
   # 日本語ファイル名の設定
   CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
-  
 end
 
 
