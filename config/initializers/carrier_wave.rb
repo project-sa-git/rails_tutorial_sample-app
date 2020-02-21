1e3088f807c324dab1a9ce931d908ce80db0cbd3
EC2 Default User <ec2-user@ip-172-31-41-189.ap-northeast-1.compute.internal>
2020-02-21 16:17:21 +0000

    fixed conflict

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,5 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.storage :fog #=> 追加
     config.fog_provider = 'fog/aws'  #=> 追加
     config.fog_credentials = {
       # Amazon S3用の設定
